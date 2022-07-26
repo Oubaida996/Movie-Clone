addf19cc4fb72554643a1c9b1ed1741b5326e2c2
Obieda Akilan <obieda.jehad@gmail.com>
2022-07-26 21:00:03 +0300

    how to use useNavigate and useLocation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Header from './component/Header/Header';
 import Body from './component/Body/Body';
 import axios from 'axios';
-import BasicPagination from './component/Pagination/Pagination';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './component/Footer/Footer';
+import DetailsMovie from './component/DetailsMovie/DetailsMovie';
 function App() {
 
   const [movies, setMovies] = useState(null);
   const [page, setPage] = useState(1);
-
+  const [mount, setMount] = useState(false)
   //============Start get data from api by axios
   const getAllMovies = async () => {
     const apiKey = `https://api.themoviedb.org/3/movie/popular?api_key=76495f291e5b44a1dae4fa6877a17d91&page=${page}`;
@@ -40,10 +41,15 @@ function App() {
 
   return (
     <div className="App">
-      <Header search={search} />
-      <Body movies={movies} />
-      <BasicPagination handlePageChange={handlePageChange} />
-      <Footer />
+      <BrowserRouter>
+        <Header search={search} />
+        <Routes>
+          <Route path="/" element={<Body movies={movies} handlePageChange={handlePageChange} />} />
+          <Route path="details/:id" element={<DetailsMovie />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/Body/Body.jsx
+++ src/component/Body/Body.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import './Body.scss'
 import CardMovie from '../CardMovie/CardMovie';
+import BasicPagination from '../Pagination/Pagination';
 
-export default function Body({ movies }) {
+export default function Body({ movies, handlePageChange }) {
 
 
 
     return (
 
         movies && <div className='kBody'>
-            {movies.map((movie, indx) => {
-                return <CardMovie key={indx} movie={movie} />
-            })}
-
+            <div className='kContent'>
+                {movies.map((movie, indx) => {
+                    return <CardMovie key={indx} movie={movie} />
+                })}
+            </div>
+            <BasicPagination handlePageChange={handlePageChange} />
         </div > || <div className='loading'>loading</div>
     )
 }
