19f34dbad0c6eb11f2b798110b71f3242997d6e5
Obieda Akilan <obieda.jehad@gmail.com>
2022-08-01 19:14:14 +0300

    add style for card details

--- src/component/DetailsMovie/DetailsMovie.jsx
+++ src/component/DetailsMovie/DetailsMovie.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './DetailsMovie.scss';
-import { useLocation } from 'react-router-dom';
-import Text from '../CardMovie/Text';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function DetailsMovie() {
     const location = useLocation();
+    const navigate = useNavigate();
+
+    const [movie, setMovie] = useState(null);
+
+    const getMovie = async () => {
+        const api = `https://api.themoviedb.org/3/movie/${location.state.id}?api_key=76495f291e5b44a1dae4fa6877a17d91&language=en-US`;
+        const res = await fetch(api);
+        const data = await res.json();
+        console.log(data);
+        setMovie(data);
+    }
+
+
+    useEffect(() => {
+        getMovie();
+    }, []);
     return (
 
         <div className='DetailsMovie' >
@@ -14,20 +29,20 @@ export default function DetailsMovie() {
                     <img src={`https://image.tmdb.org/t/p/w500${location.state.poster_path}`} alt="img" />
                 </div>
                 <div className='kInfo'>
-                    <div className='kTitle'>
-                        <h3>{location.state.title}</h3>
-                    </div>
-                    <Text text={`Release Date : ${location.state.release_date}`} />
-                    <Text text={`Vote Count : ${location.state.vote_count}`} />
-                    <Text text={`Vote Average : ${location.state.vote_average}`} />
-
-
-
+                    <h3>{location.state.title}</h3>
+                    <p><strong>Release Date :</strong>{` ${location.state.release_date}`}</p>
+                    <p><strong>Vote Count :</strong>{` ${location.state.vote_count}`}</p>
+                    <p><strong>Vote Average :</strong>{` ${location.state.vote_average}`}</p>
                 </div>
             </div >
             <div className='kOverview'>
                 <h3>Overview</h3>
                 <p>{location.state.overview}</p>
+            </div>
+            <div className='kButtons'>
+
+                <button onClick={() => { navigate(`/`) }}>Home</button>
+                {movie && <a href={`${movie.homepage}`} target='_blank' ><button >Play</button></a>}
             </div>
         </div >
     )
